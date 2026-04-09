719bb83106aa334bb527909134f395e939012468
service-rgbd <perriermathilde170@gmail.com>
2026-04-09 18:11:04 +0000

    Fix dashboard recharts imports

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { 
-  AreaChart, Area 
+  AreaChart, Area, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis
 } from 'recharts';
 import { FleetStats, RevenueData } from '../types';
 import { 
